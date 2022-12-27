a0953104fe6d5695343c3cf68674963167deb87d
seungbeen <seungbin803@gmail.com>
2022-12-27 19:00:43 +0900

    Java 기초 복습

---
+++ src/VariableInitializationEx.java
@@ -0,0 +1,8 @@
+public class VariableInitializationEx {
+    public static void main(String[] args) {
+        int value = 50;
+
+        int result = value + 10;
+        System.out.println(result);
+    }
+}
